107df2a50566bcf392e6cc97776797088fe7eb37
Andrei D2u <andreidavidoiu@gmail.com>
2025-11-10 19:02:42 +0100

    update2

--- app/components/social/SocialComponent.tsx
+++ app/components/social/SocialComponent.tsx
@@ -1,75 +1,7 @@
-import {
-  get_country_comments,
-  get_landing_comments,
-  get_region_comments,
-  get_takeoff_comments,
-} from "@/app/api/get/get_comments";
-import SocialView from "./SocialView";
-import type { Comment } from "@/app/types";
-import { get_profiles } from "./action";
+import React from "react";
 
-type Profile = {
-  id: number;
-  url: string;
+const SocialComponent = () => {
+  return <div>SocialComponent</div>;
 };
 
-type CommentWithProfile = Comment & { profileId: number };
-
-type CommentsResponse = {
-  userID: string | null;
-  comments: CommentWithProfile[];
-};
-
-type SocialComponentProps = {
-  selectedTipe: "c" | "r" | "t" | "l";
-  selectedId: number;
-  selectedName: string;
-};
-
-export default async function SocialComponent({
-  selectedTipe,
-  selectedId,
-  selectedName,
-}: SocialComponentProps) {
-  let rez: CommentsResponse;
-
-  if (selectedTipe === "c")
-    rez = (await get_country_comments({ id: selectedId })) ?? {
-      userID: null,
-      comments: [],
-    };
-  else if (selectedTipe === "r")
-    rez = (await get_region_comments({ id: selectedId })) ?? {
-      userID: null,
-      comments: [],
-    };
-  else if (selectedTipe === "t")
-    rez = (await get_takeoff_comments({ id: selectedId })) ?? {
-      userID: null,
-      comments: [],
-    };
-  else
-    rez = (await get_landing_comments({ id: selectedId })) ?? {
-      userID: null,
-      comments: [],
-    };
-
-  // Extrage profileId-uri unice
-  const list: number[] = Array.from(
-    new Set(rez.comments.map((item) => item.profileId))
-  );
-
-  // Preia profilele
-  const profiles: Profile[] = await get_profiles(list);
-
-  return (
-    <SocialView
-      selectedTipe={selectedTipe}
-      selectedId={selectedId}
-      selectedName={selectedName}
-      comments={rez.comments}
-      user={rez.userID}
-      profiles={profiles}
-    />
-  );
-}
+export default SocialComponent;
